2f54dfd3524f44aef96b2eccd74e841ce99a32c8
Ivan Indrautama <ivanindrautama@gmail.com>
2024-04-02 14:54:44 -0500

    add updating to relative path script

--- update_to_relative_path.py
+++ update_to_relative_path.py
@@ -25,7 +25,7 @@ unique_id = uuid.uuid5(uuid.NAMESPACE_DNS, host_name + str(uuid.getnode()))
 
 # Configure logging
 log_app_name = "app"
-log_level = os.getenv('LOG_LEVEL', 'DEBUG')
+log_level = os.getenv('LOG_LEVEL', 'INFO')
 log_level = getattr(logging, log_level.upper())
 
 file_handler = RotatingFileHandler(f"{log_app_name}_{unique_id}.log", maxBytes=10485760, backupCount=10)
@@ -57,6 +57,7 @@ FILELIST_CACHE_FILENAME = os.getenv('CACHE_FILENAME', 'filelist_cache.msgpack')
 SOURCE_IMAGE_DIRECTORY = os.getenv('IMAGE_DIRECTORY', 'images')
 CHROMA_DB_PATH = os.getenv('CHROME_PATH', f"{DATA_DIR}/{unique_id}_chroma")
 CHROMA_COLLECTION_NAME = os.getenv('CHROME_COLLECTION', "images")
+CLIP_MODEL = os.getenv('CLIP_MODEL', "openai/clip-vit-base-patch32")
 
 logger.debug("Configuration loaded.")
 # Log the configuration for debugging
@@ -66,68 +67,35 @@ logger.debug(f"Configuration - CACHE_FILENAME: {FILELIST_CACHE_FILENAME}")
 logger.debug(f"Configuration - IMAGE_DIRECTORY: {SOURCE_IMAGE_DIRECTORY}")
 logger.debug(f"Configuration - CHROME_PATH: {CHROMA_DB_PATH}")
 logger.debug(f"Configuration - CHROME_COLLECTION: {CHROMA_COLLECTION_NAME}")
+logger.debug(f"Configuration - CLIP_MODEL: {CLIP_MODEL}")
 logger.debug("Configuration loaded.")
 
 # Append the unique ID to the db file path and cache file path
 SQLITE_DB_FILEPATH = f"{DATA_DIR}{str(unique_id)}_{SQLITE_DB_FILENAME}"
-FILELIST_CACHE_FILEPATH = os.path.join(DATA_DIR, f"{unique_id}_{FILELIST_CACHE_FILENAME}")
-logger.info(f"Using SQLite DB: {SQLITE_DB_FILEPATH}")
-# Initialize or get the Chroma collection
-logger.info(f"Initializing Chrome DB:  {CHROMA_COLLECTION_NAME}")
-client = chromadb.PersistentClient(path=CHROMA_DB_PATH)
-collection = client.get_or_create_collection(name=CHROMA_COLLECTION_NAME)
 
-def get_db_connection():
-    conn = sqlite3.connect(SQLITE_DB_FILEPATH)
-    conn.row_factory = sqlite3.Row  # Access columns by names
-    return conn
-
-def load_embeddings():
-    """Load photos and their embeddings from the SQLite database."""
-    memes = []
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT id, filename, file_path, embeddings FROM images WHERE embeddings IS NOT NULL")
-    rows = cursor.fetchall()
-    for row in rows:
-        embeddings = msgpack.unpackb(row["embeddings"], raw=False)
-        memes.append({
-            "id": row["filename"],
-            "file_path": row["file_path"],
-            "embeddings": embeddings
-        })
-    return memes
-
-images = load_embeddings()
-print(f"Loaded {len(images)} photos from the database that have embeddings.")
+def replace_file_path_in_db():
+    """
+    Replaces the DATA_DIR value with an empty string in the file_path field for all rows in the images table.
+    """
+    try:
+        with sqlite3.connect(SQLITE_DB_FILEPATH) as conn:
+            # Prepare the SQL statement for updating the file_path
+            update_sql = "UPDATE images SET file_path = REPLACE(file_path, '{}', '')".format(SOURCE_IMAGE_DIRECTORY)
+            print(update_sql)
+            # Execute the update statement, replacing DATA_DIR with an empty string
+            conn.execute(update_sql)
+            logger.info("Successfully replaced file_path to relateive path of DATA_DIR for all images.")
+    except sqlite3.Error as e:
+        logger.error(f"Failed to replace file_path to relateive path of DATA_DIR for images: {e}")
 
 
-# items = collection.peek()
-
-items = collection.get()["ids"]
-
-print(f"Loaded {len(items)} items from Chroma DB")
-
-# for i in items:
-    # print(i)
-
-start_time = time.time()
-if len(items) <= len(images):
 
 
-    for image in images:
+def main():
+    # Call the new function to replace DATA_DIR in file_path
+    replace_file_path_in_db()
 
-        if image['id'] in items:
-            print(f"Skipping {image['id']}")
-            continue
-        print(f"adding {image['id']}")
-        collection.add(
-            embeddings=image["embeddings"],
-            documents=[ image['id']],
-            metadatas=[{"id":image['id'], "file_name": image['id'], "path": image['file_path']}],
-            ids=[image['id']]
-        )
-        # print(f"Added embedding to Chroma for {m['id']}")
+    
 
-end_time = time.time()
-print(f"Added {len(images)} embeddings to Chroma DB in {end_time - start_time} seconds")
+if __name__ == "__main__":
+    main()
